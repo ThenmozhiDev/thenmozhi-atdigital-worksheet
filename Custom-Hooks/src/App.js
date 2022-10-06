6f3e37acf69f434908259639fefda0177f598c1b
ThenmozhiDev <thenmozhijos2021@gmail.com>
2022-10-06 19:02:29 +0530

    Custom-hook project

---
+++ Custom-Hooks/src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+
+import Task from './components/Task/Task';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './Hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://react-http-ffed2-default-rtdb.firebaseio.com/tasks.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Task
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+{/*import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
+
+function App() {
+  return (
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
+  );
+}
+
+export default App; */}
---
+++ Custom-Hooks/src/components/Task/Task.js
@@ -0,0 +1,36 @@
+import Section from '../UI/Section';
+import TaskItem from './TaskItem';
+import classes from './Task.module.css';
+
+const Task = (props) => {
+
+  let taskList = <h2>No tasks found. Start adding some!</h2>;
+  
+  if (props.items.length > 0) {
+    taskList = (
+      <ul>
+        {props.items.map((task) => (
+          <TaskItem key={task.id}>{task.text}</TaskItem>
+        ))}
+      </ul>
+    );
+  }
+
+  let content = taskList;
+
+  if (props.error) {
+    content = <button onClick={props.onFetch}>Try again</button>;
+  }
+
+  if (props.loading) {
+    content = 'Loading tasks...';
+  }
+
+  return (
+    <Section>
+      <div className={classes.container}>{content}</div>
+    </Section>
+  );
+};
+
+export default Task;
