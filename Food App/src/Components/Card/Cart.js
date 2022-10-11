7521a6142db860f6370262e9b5ead356427363f1
ThenmozhiDev <thenmozhijos2021@gmail.com>
2022-10-11 19:31:59 +0530

    react-redux created

--- Food App/src/Components/Card/Cart.js
+++ Food App/src/Components/Card/Cart.js
@@ -1,10 +1,12 @@
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "../../Store/CartContext";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+  const [isCheckout, setIsCheckout] = useState(false);
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -16,6 +18,10 @@ const Cart = (props) => {
   const cartItemAddHandler = (item) => {
     cartCtx.addItem(item);
   };
+
+  const orderHandler = () => {
+setIsCheckout(true);
+  }
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -32,6 +38,15 @@ const Cart = (props) => {
     </ul>
   );
   console.log(cartItems);
+
+  const modalCheckout = (
+    <div className={classes.actions}>
+    <button className={classes["button--art"]} onClick={props.onClose}>
+      Close
+    </button>
+    {hasItems && <button className={classes.button} onClick={orderHandler} >Order</button>}
+  </div>
+  )
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
@@ -39,12 +54,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--art"]} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+     {isCheckout && <Checkout onCancel={props.onClose}/>}
+      {!isCheckout && modalCheckout}
     </Modal>
   );
 };
--- Food App/src/Components/Meals/AvailableMeals.js
+++ Food App/src/Components/Meals/AvailableMeals.js
@@ -1,36 +1,60 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealsItem from "./MealsItem/MealsItem";
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading ] = useState(false);
+  const [httpError, setHttpError] = useState()
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch("https://react-http-ffed2-default-rtdb.firebaseio.com/meals.json");
+
+      if(!response.ok) {
+        throw new Error("something went wrong!");
+      }
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        })
+
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      }
+    };
+  
+      fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+  }, []);
+
+  if(isLoading){
+    return <section >
+      <p className={classes.MealsLoading}>Loading...</p>
+    </section>
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealsItem
       key={meal.id}
       id={meal.id}
