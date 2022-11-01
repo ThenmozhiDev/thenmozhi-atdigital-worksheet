d726a6a946af9f83661717a9709a102b5b07cbd9
ThenmozhiDev <thenmozhijos2021@gmail.com>
2022-11-01 19:21:30 +0530

    summary-project

---
+++ react-project-5/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+import { FavoritesContextProvider } from './Store/FavoritesContext';
+
+ReactDOM.render(
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>,
+  document.getElementById('root')
+);
